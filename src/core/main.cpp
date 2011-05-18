5835f7bb9de4fd868ea12bb660afe3ef010296e2
Claudio Valerio <claudio@sente.ch>
2011-05-18 17:44:07 +0200

    Removed google-breakpad

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -11,50 +11,6 @@
 #include "UBApplication.h"
 #include "UBSettings.h"
 
-#ifdef Q_WS_MAC
-#include "google_breakpad/client/mac/handler/exception_handler.h"
-#elif defined(Q_WS_WIN)
-#include "google_breakpad/client/windows/handler/exception_handler.h"
-#else
-// TODO UB 4.x - Linux
-#endif
-
-#ifdef Q_WS_MAC
-bool MacCallback(const char *dump_dir, const char *file_name,
-		void *context, bool success) {
-
-	Q_UNUSED(context);
-	Q_UNUSED(success);
-	std::string path(dump_dir);
-	path.append(file_name);
-	path.append(".dmp");
-	qDebug() << "minidump has been created " << QString::fromStdString(path);
-	// Currently we let OS alert the user about the crash. We will return true when
-	// message to the user will be implemented. But dump is available in {data directory}/log
-	return false;
-}
-#elif defined(Q_WS_WIN)
-bool WinCallBack(const wchar_t* dump_path,
-		const wchar_t* minidump_id,
-		void* context,
-		EXCEPTION_POINTERS* exinfo,
-		MDRawAssertionInfo* assertion,
-		bool succeeded) {
-	Q_UNUSED(context);
-	Q_UNUSED(exinfo);
-	Q_UNUSED(assertion);
-	Q_UNUSED(succeeded);
-	std::wstring path(dump_path);
-	path.append(L"/");
-	path.append(minidump_id);
-	path.append(L".dmp");
-	qDebug() << "minidump has been created " << QString::fromStdWString(path);
-	// Currently we let OS alert the user about the crash. We will return true when
-	// message to the user will be implemented. But dump is available in {data directory}/log
-	return false;
-}
-#endif
-
 void ub_message_output(QtMsgType type, const char *msg) {
 	// We must temporarily remove the handler to avoid the infinite recursion of
 	// ub_message_output -> qt_message_output -> ub_message_output -> qt_message_output ...
@@ -113,12 +69,6 @@ int main(int argc, char *argv[]) {
 	QDir logDir(dumpPath);
 	if (!logDir.exists())
 		logDir.mkdir(dumpPath);
-
-#if defined(Q_WS_MAC) && defined(QT_NO_DEBUG)
-	google_breakpad::ExceptionHandler breakpadHandler(dumpPath.toStdString(), 0, MacCallback, 0, true);
-#elif defined(Q_WS_WIN)
-	google_breakpad::ExceptionHandler breakpadHandler(dumpPath.toStdWString(), 0, WinCallBack, 0, google_breakpad::ExceptionHandler::HANDLER_ALL);
-#endif
 
 	QString fileToOpen;
 
--- thirdparty/libs.pri
+++ thirdparty/libs.pri
@@ -21,16 +21,7 @@ INCLUDEPATH += "$$XPDF_DIR"
 INCLUDEPATH += "$$XPDF_DIR/goo"
 INCLUDEPATH += "$$XPDF_DIR/splash"
 
-BREAKPAD_DIR = "$$PWD/google-breakpad/r318"
-INCLUDEPATH += "$$BREAKPAD_DIR/include" "$$BREAKPAD_DIR/include/google_breakpad"
-
 win32 {
-CONFIG(debug, debug|release) {
-        LIBS        += "-L$$BREAKPAD_DIR/lib/$$SUB_LIB" "-ldbreakpad"
-}
-CONFIG(release, debug|release) {
-        LIBS        += "-L$$BREAKPAD_DIR/lib/$$SUB_LIB" "-lbreakpad"
-}
     LIBS        += "-L$$PWD/openssl/0.9.8i/lib/VC/static" "-llibeay32MD"
     INCLUDEPATH += "$$PWD/openssl/0.9.8i/include"
 
@@ -57,7 +48,6 @@ macx {
     LIBS         += "-framework Carbon"
             
     LIBS         += "-lcrypto"
-    LIBS         += "-L$$BREAKPAD_DIR/lib/$$SUB_LIB" "-lbreakpad"
     LIBS         += "$$PWD/unsanity/ape/APELite.o"
     INCLUDEPATH  += "$$PWD/unsanity/ape"
 }
