73555ef012c4e1d695200e19909d284ff2d7701e
Claudio Valerio <claudio@sente.ch>
2011-08-02 13:43:57 +0200

    Added lgpl header

--- src/adaptors/voting/UBReply2005VotingSystem.cpp
+++ src/adaptors/voting/UBReply2005VotingSystem.cpp
@@ -1,8 +1,16 @@
 /*
- * UBReply2005VotingSystem.cpp
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *  Created on: 12 feb. 2010
- *      Author: Luc
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "UBReply2005VotingSystem.h"
--- src/pdf-merger/PageParser.h
+++ src/pdf-merger/PageParser.h
@@ -1,8 +1,17 @@
-///////////////////////////////////////////////////////////
-//  Page.h
-//  Implementation of the Class Page
-//  Created on:      19-θών-2009 12:27:56
-///////////////////////////////////////////////////////////
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #if !defined(EA_FF53E894_11D7_4c64_A409_DBC97C9EF3CF__INCLUDED_)
 #define EA_FF53E894_11D7_4c64_A409_DBC97C9EF3CF__INCLUDED_
