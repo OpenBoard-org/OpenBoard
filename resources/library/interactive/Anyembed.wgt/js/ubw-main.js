f5cff37eff5c69b71e534b518e4421c17ca62bb5
unknown <D:\mail>
2011-10-19 17:20:55 +0300

    upgraded AnyEmbed, VideoPicker and WebBrowser apps

--- resources/library/interactive/Anyembed.wgt/js/ubw-main.js
+++ resources/library/interactive/Anyembed.wgt/js/ubw-main.js
@@ -14,84 +14,89 @@
 */
 
 function init(){
-	var embed;
-	var inputBox = $("<textarea type='text'></textarea>")
-	.css({
-		width: "350px",
-		height: "58px",
-		marginRight: "5px"
-	});
+    var embed;
+    var inputBox = $("<textarea type='text'></textarea>")
+    .css({
+        width: "350px",
+        height: "58px",
+        marginRight: "5px"
+    });
 
-	var submit = $("<div>Embed</div>")
-	.css({	
-		float: "right",
-		width: "auto",
-		height:"auto",
-		padding:"4px",
-		marginTop:"13px",
-		cursor:"pointer",
-		backgroundColor:"rgb(45,65,250)",
-		border:"1px solid",
-		borderColor:"rgb(45,65,250) rgb(25,45,230) rgb(25,45,230) rgb(45,65,250)",
-		textTransform:"capitalize",
-		fontFamily:"Arial",
-		fontSize:"18px",
-		fontWeight:"regular",
-		color:"white"
-	});
+    var submit = $("<div>Embed</div>")
+    .css({	
+        float: "right",
+        width: "auto",
+        height:"auto",
+        padding:"4px",
+        marginTop:"13px",
+        cursor:"pointer",
+        backgroundColor:"rgb(45,65,250)",
+        border:"1px solid",
+        borderColor:"rgb(45,65,250) rgb(25,45,230) rgb(25,45,230) rgb(45,65,250)",
+        textTransform:"capitalize",
+        fontFamily:"Arial",
+        fontSize:"18px",
+        fontWeight:"regular",
+        color:"white"
+    });
 	
-	var loadWindow = $("<div></div>");
+    var loadWindow = $("<div></div>");
 	
-	var searchWrap = $("<div></div>")
-	.css({
-		position:"absolute",
-		padding: "10px",
-		backgroundColor: "rgb(230,230,233)",
-		width: "425px",
-		height: "65px"
-	});
+    var searchWrap = $("<div></div>")
+    .css({
+        position:"absolute",
+        padding: "10px",
+        backgroundColor: "rgb(230,230,233)",
+        width: "425px",
+        height: "65px"
+    });
 	
-	//FIT WIDGET FRAME ON LAUNCH...
-	if(window.sankore){
-		window.sankore.resize($("#ubwidget").width()-15,85);
-	}
+    //FIT WIDGET FRAME ON LAUNCH...
+    if(window.sankore){
+        window.sankore.resize($("#ubwidget").width()-15,85);
+    }
 	
-	searchWrap.append(inputBox)
-			  .append(submit);
+    searchWrap.append(inputBox)
+    .append(submit);
 			
-	$("#ubwidget").append(searchWrap);	
+    $("#ubwidget").append(searchWrap);	
 
-	submit.click(function(){
-		$("#ubwidget").append(loadWindow)
-		embed = inputBox.val();
-		searchWrap.hide();
+    submit.click(function(){
+        $("#ubwidget").append(loadWindow)
+        embed = inputBox.val();
+        searchWrap.hide();
+
+        if(/<object/.test(embed)){
+            loadWindow.append(embed);
+            loadWindow.find("embed").attr("wmode","opaque");
+            var loadWindow_content = loadWindow.html();
+            loadWindow.empty().html(loadWindow_content);
+        }
+        else {
+            loadWindow.append(embed);				
+        }
+
+        if(window.sankore){
+            window.sankore.resize($(document).width(),$(document).height());
+            window.sankore.setPreference("embed", escape(embed));
+        }
 		
-		if(/<object/.test(embed)){
-			loadWindow.append(embed);
-			loadWindow.find("embed").attr("wmode","opaque");
-			var loadWindow_content = loadWindow.html();
-			loadWindow.empty().html(loadWindow_content);
-		}
-		else {
-			loadWindow.append(embed);				
-		}
-
-		if(window.sankore){
-			window.sankore.resize($(document).width(),$(document).height());
-			window.sankore.setPreference("embed", escape(embed));
-		}
-		
-	});
+    });
 	
-	if(window.sankore){
+    if(window.sankore){
 
-		if(window.sankore.preferenceKeys().length != 0){
+        if(window.sankore.preferenceKeys().length != 0){
 
-				var loadEmbed = unescape(window.sankore.preference("embed"));
-				inputBox.val(loadEmbed);
-				submit.trigger("click");	
-		}	
-	
-	}
+            var loadEmbed = unescape(window.sankore.preference("embed"));
+            inputBox.val(loadEmbed);
+            submit.trigger("click");	
+        }	
+    }
+    
+    if (window.widget) {
+        window.widget.onremove = function(){
+            loadWindow.empty();
+        }
+    }
 
 }
