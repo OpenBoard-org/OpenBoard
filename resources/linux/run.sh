2f441229557127039c6c20b7564b1c649b061b5f
Claudio Valerio <claudio@open-sankore.org>
2011-12-01 12:07:23 +0100

    changed run.sh script file

--- resources/linux/run.sh
+++ resources/linux/run.sh
@@ -14,4 +14,4 @@
 # ---------------------------------------------------------------------
 
 #!/bin/sh
-env LD_LIBRARY_PATH=$PWD/qtlib:$LD_LIBRARY_PATH ./Sankore\ 3.1 
+env LD_LIBRARY_PATH=$PWD/qtlib:$LD_LIBRARY_PATH ./Open-Sankoré 
