ce8c7bee2f6b960df4f52913990b426f2ee62e22
Anatoly Mihalchenko <tolik@scand.com>
2012-01-10 15:03:28 +0300

    http://188.165.53.52/jira/browse/SANKORE-450

--- src/board/UBBoardController.cpp
+++ src/board/UBBoardController.cpp
@@ -1591,6 +1591,8 @@ void UBBoardController::setPageSize(QSize newSize)
         updatePageSizeState();
         adjustDisplayViews();
         mActiveDocument->setMetaData(UBSettings::documentUpdatedAt, UBStringUtils::toUtcIsoDateTime(QDateTime::currentDateTime()));
+
+        UBSettings::settings()->pageSize->set(newSize);
     }
 }
 
