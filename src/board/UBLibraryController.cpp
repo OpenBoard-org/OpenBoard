17f3e1a8eab4bd52ea58f5a411c86e468ce37688
Aleksei Kanash <sc.kanash.aleksei@gmail.com>
2012-02-09 15:35:12 +0200

    Fix to SANKORE-307

--- src/board/UBLibraryController.cpp
+++ src/board/UBLibraryController.cpp
@@ -649,11 +649,11 @@ QRectF UBLibraryController::visibleSceneRect()
 
 void UBLibraryController::addImagesToCurrentPage(const QList<QUrl>& images)
 {
-    QPointF pos = visibleSceneRect().center();
+    QPointF pos = UBApplication::boardController->activeScene()->normalizedSceneRect().center();
 
+   
     foreach(const QUrl url, images)
     {
-        mLastItemOffsetIndex++;
         mLastItemOffsetIndex = qMin(mLastItemOffsetIndex, 5);
 
         QGraphicsItem* itemInScene = 0;
@@ -662,6 +662,8 @@ void UBLibraryController::addImagesToCurrentPage(const QList<QUrl>& images)
             QString mimeType = UBFileSystemUtils::mimeTypeFromFileName(
                     url.toString());
 
+            pos = QPointF(pos.x() + 50 * mLastItemOffsetIndex, pos.y() + 50 * mLastItemOffsetIndex);
+            mLastItemOffsetIndex++;
             //TODO UB 4.x move this logic to the scene ..
             if (mimeType == "image/svg+xml") {
                 itemInScene = activeScene()->addSvg(url, pos);
@@ -673,8 +675,6 @@ void UBLibraryController::addImagesToCurrentPage(const QList<QUrl>& images)
 
         if (itemInScene) {
             itemInScene = activeScene()->scaleToFitDocumentSize(itemInScene, false, UBSettings::objectInControlViewMargin);
-
-            itemInScene->setPos(QPoint(pos.x() + 50 * mLastItemOffsetIndex, pos.y() + 50 * mLastItemOffsetIndex));
         }
     }
 }
