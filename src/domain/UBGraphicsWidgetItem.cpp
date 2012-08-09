f3496036d5d69b2b1e5e696494435936dca4dc56
Yimgo <yimgo@yimgo.fr>
2012-08-09 13:47:51 +0200

    Fixed widget paint when non-scene rendering.

--- src/domain/UBGraphicsWidgetItem.cpp
+++ src/domain/UBGraphicsWidgetItem.cpp
@@ -889,9 +889,11 @@ void UBGraphicsW3CWidgetItem::paint(QPainter * painter, const QStyleOptionGraphi
       rc =  scene()->renderingContext();
 
     if (rc == UBGraphicsScene::NonScreen || rc == UBGraphicsScene::PdfExport) {
-        takeSnapshot();
-        if (!snapshot().isNull())
-           painter->drawPixmap(0, 0, snapshot());
+
+        if (snapshot().isNull())
+            takeSnapshot();
+           
+        painter->drawPixmap(0, 0, snapshot());
     }
     else
         UBGraphicsWidgetItem::paint(painter, option, widget);
