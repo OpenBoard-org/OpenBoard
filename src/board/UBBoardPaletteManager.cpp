f25da1f3eca4e15f88da33bac5665ff9fd7f6d88
shibakaneki <didier@sente.ch>
2012-08-10 08:52:36 +0200

    Fixed issue 633

--- src/board/UBBoardPaletteManager.cpp
+++ src/board/UBBoardPaletteManager.cpp
@@ -681,8 +681,10 @@ void UBBoardPaletteManager::changeMode(eUBDockPaletteWidgetMode newMode, bool is
     {
         case eUBDockPaletteWidget_BOARD:
             {
-                mLeftPalette->assignParent(UBApplication::boardController->controlContainer());
-                mRightPalette->assignParent(UBApplication::boardController->controlContainer());
+                mLeftPalette->assignParent(mContainer);
+                mRightPalette->assignParent(mContainer);
+                mRightPalette->stackUnder(mStylusPalette);
+                mLeftPalette->stackUnder(mStylusPalette);
                 if (UBPlatformUtils::hasVirtualKeyboard() && mKeyboardPalette != NULL)
                 {
 
@@ -714,6 +716,8 @@ void UBBoardPaletteManager::changeMode(eUBDockPaletteWidgetMode newMode, bool is
             {
                 mLeftPalette->assignParent((QWidget*)UBApplication::applicationController->uninotesController()->drawingView());
                 mRightPalette->assignParent((QWidget*)UBApplication::applicationController->uninotesController()->drawingView());
+                mRightPalette->lower();
+                mLeftPalette->lower();
                 if (UBPlatformUtils::hasVirtualKeyboard() && mKeyboardPalette != NULL)
                 {
 
