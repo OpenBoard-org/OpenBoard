694e6e5a64c657f39ca3fff5316debff014d3819
Claudio Valerio <claudio@open-sankore.org>
2012-08-16 09:43:33 +0200

    fixed group ungroup button 2

--- src/board/UBBoardController.cpp
+++ src/board/UBBoardController.cpp
@@ -880,13 +880,11 @@ void UBBoardController::groupButtonClicked()
     }
 
     if (groupAction->text() == UBSettings::settings()->actionGroupText) { //The only way to get information from item, considering using smth else
-    	groupAction->setIcon(QIcon(":images/toolbar/ungroup.png"));
     	UBGraphicsGroupContainerItem *groupItem = activeScene()->createGroup(selItems);
         groupItem->setSelected(true);
         UBDrawingController::drawingController()->setStylusTool(UBStylusTool::Selector);
 
     } else if (groupAction->text() == UBSettings::settings()->actionUngroupText) {
-    	groupAction->setIcon(QIcon(":images/toolbar/group.png"));
         //Considering one selected item and it's a group
         if (selItems.count() > 1) {
             qDebug() << "can't make sense of ungrouping more then one item. Grouping action should be performed for that purpose";
