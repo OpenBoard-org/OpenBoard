3adc47399c73985a8c67a51b3de13870b0d906e7
Claudio Valerio <claudio@open-sankore.org>
2012-10-09 10:32:09 +0200

    moved njin widgets that doesn't work

---
+++ resources/nonDistributed/Transformation.wgt/js/help_fr-template.js
@@ -0,0 +1,20 @@
+<h3>Boîte à transformation</h3>
+<h4>Emettre des hypothèses</h4>
+
+<p>Les cartes passent à travers une boîte et subissent une action. Le but est d’identifier l'opération effectuée par cette dernière en utilisant un minimum de cartes.</p> 
+<p>Pour faire passer une carte à travers la boîte, il suffit de cliquer sur celle-ci.</p>
+
+<p>Les hypothèses peuvent être saisies sur le tableau (en dehors de l'interactivité).</p>
+
+<p>Le bouton “Recharger” réinitialise l'exercice.</p>
+
+<p>Le bouton "Modifier" vous permet :</p>
+<ul><li>de choisir le thème de l’interactivité : tablette, ardoise ou aucun (par défaut tablette),</li>
+<li>de déterminer le nombre de cartes que vous voulez utiliser,</li>
+<li>de déterminer les cartes avant et après transformation.</li></ul>
+
+<p>Les cartes possèdent par défaut un champ texte.</p> 
+<p>Pour insérer du texte, cliquez sur la carte et saisissez le texte souhaité.</p> 
+<p>Pour ajouter une image depuis votre bibliothèque, cochez la case image et utilisez le glisser-déposer depuis votre bibliothèque jusqu’à l’emplacement prévu.</p>
+
+<p>Le bouton “Afficher” vous permet d’utiliser l’activité.</p>
