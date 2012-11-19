1f1fe9844ad7f0577dcb6b934e0e1d68c224a934
bmagnin <vi>
2012-11-19 22:20:29 +0100

    inter activities en help : tables, train, transformation

--- resources/library/interactivities/Tables.wgt/js/help-template.js
+++ resources/library/interactivities/Tables.wgt/js/help-template.js
@@ -1,13 +1,17 @@
-<h3>Table</h3>
-<h4>Mental calculation</h4>
+<h3> Table</h3>
+<h4> Mental calculation, properties of operations </h4>
 
 <p>With the table App you can work on properties of the various operations and review tables of addition, subtraction, multiplication, and division. Click on the case to display or to hide the answer.</p>
 
-<p>The "Edit" button allows you to choose :</p>
-<ul>
-	<li>the theme of the App : tablet, slate, or none (by default tablet).</li>
-	<li>the operation,</li>
-	<li>the number of line and column (1-12),</li>
-	
-</ul>
 <p>The arrow on the board indicates the sense of reading.</p>
+
+<p> Reload "button resets the exercise. </p>
+
+
+<p> Enter the "Edit" mode to choose the : </p>
+<ul>
+<ul> <li>  theme of interactivity : tablet, slate or none (by default : tablet),</li>
+<li> desired operation: addition, subtraction, multiplication or division,</ li>
+<li> number of rows and columns (1 to 12). </li> </ul>
+
+<p> "Display" button comes back to the activity.</p>
