24b42c984f6a7358395ccf123a9a31ede03777f0
bmagnin <vi>
2012-11-19 17:00:57 +0100

    Interactivity help : cadran, calcul

--- resources/library/interactivities/Cadran.wgt/js/help-template.js
+++ resources/library/interactivities/Cadran.wgt/js/help-template.js
@@ -1,1 +1,13 @@
-<h3>Le disque</h3>
+<h3>Dial</h3>
+<h4>Mental calculation</h4>
+
+<p> With this interactivity you can work a particular number (number of the days) with additions, subtractions, multiplications and divisions. The goal is to train mental calculation clicking on the dial and on the question mark. </p>
+
+<p> "Reload" button resets the selection. </p>
+<p> Enter the "Edit" mode to : </p>
+<ul> <li> choose the theme of the App : tablet, slate, or none (by default : slate), </li>
+<li> modify the number in the center of the dial. </li> </ul>
+
+<p> To change the number in the center, click and replace it.</p>
+
+<p>"Display" button comes back to the activity.</p>
--- resources/library/interactivities/Calcul.wgt/js/help-template.js
+++ resources/library/interactivities/Calcul.wgt/js/help-template.js
@@ -1,1 +1,17 @@
-<h3>Calcul rapide</h3>
+<h3> Calculation </h3>
+<h4> Arithmetic, simple operations </h4>
+ 
+<p>Interactivity trains add, subtract and multiply numbers. The goal is to enter the correct results. To check the answer click on the "v" button. If the result is incorrect, the calculation turns in red. If the result is correct, the calculation turns in green. </p>
+<p>Once the series is performed, the interactivity reviews all calculations</p>
+ 
+<p>"Reload" button generates new calculations. </p>
+ 
+<p>Enter the "Edit" mode to choose the : </p>
+<ul> <li>the theme of the App : tablet, slate, or none (by default : slate), </li>
+<li> operation (addition, subtraction, multiplication),</li>
+<li> magnitude of proposed numbers,</li>
+<li> number of elements (2-4),</li>
+<li> number of operations (1-5). </li> </ul>
+
+<p> Results are automatically calculated by the interactivity. </p>
+<p>"Display" button comes back to the activity.</p>
