c1423f9b940ac768f006594949debc40e85afdea
Claudio Valerio <claudio@open-sankore.org>
2013-05-06 10:59:11 +0200

    created the thumbnail on dnd from the board

--- src/domain/UBGraphicsSvgItem.cpp
+++ src/domain/UBGraphicsSvgItem.cpp
@@ -98,14 +98,17 @@ QVariant UBGraphicsSvgItem::itemChange(GraphicsItemChange change, const QVariant
 
 void UBGraphicsSvgItem::mousePressEvent(QGraphicsSceneMouseEvent *event)
 {
-    if (Delegate()->mousePressEvent(event))
-    {
-        //NOOP
-    }
-    else
-    {
+    QMimeData* pMime = new QMimeData();
+    QPixmap pixmap = toPixmapItem()->pixmap();
+    pMime->setImageData(pixmap.toImage());
+    Delegate()->setMimeData(pMime);
+    qreal k = (qreal)pixmap.width() / 100.0;
+
+    QSize newSize((int)(pixmap.width() / k), (int)(pixmap.height() / k));
+
+    Delegate()->setDragPixmap(pixmap.scaled(newSize, Qt::IgnoreAspectRatio, Qt::SmoothTransformation));
+    if (!Delegate()->mousePressEvent(event))
         QGraphicsSvgItem::mousePressEvent(event);
-    }
 }
 
 
