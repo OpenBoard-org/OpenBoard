19cfe0ac1c87e8f29e8e77a7c3966779a272531a
Craig Watson <craig@watsons.ch>
2016-04-04 10:03:47 +0200

    Revert "Remove polygonItems from their Stroke when added to a StrokesGroup"
    This reverts commit 177571ca79cbd92f6544c80ce36ded88ce64fa7a.

--- src/domain/UBGraphicsPolygonItem.cpp
+++ src/domain/UBGraphicsPolygonItem.cpp
@@ -114,9 +114,6 @@ UBGraphicsPolygonItem::~UBGraphicsPolygonItem()
 
 void UBGraphicsPolygonItem::setStrokesGroup(UBGraphicsStrokesGroup *group)
 {
-    if (mStroke)
-        mStroke->remove(this);
-
     mpGroup = group;
 }
 
