e811f73da8273896de7a69c41903634b16e8f4b3
Craig Watson <craig@watsons.ch>
2016-04-26 09:32:08 +0200

    Fix for stroke transparency issue
    (when an opque stroke was in the vicinity of a translucent one, it
    became translucent itself)

--- src/domain/UBGraphicsPolygonItem.cpp
+++ src/domain/UBGraphicsPolygonItem.cpp
@@ -208,7 +208,7 @@ void UBGraphicsPolygonItem::copyItemParameters(UBItem *copy) const
 void UBGraphicsPolygonItem::paint ( QPainter * painter, const QStyleOptionGraphicsItem * option, QWidget * widget)
 {
     if(mHasAlpha && scene() && scene()->isLightBackground())
-        painter->setCompositionMode(QPainter::CompositionMode_Darken);
+        painter->setCompositionMode(QPainter::CompositionMode_SourceOver);
 
     painter->setRenderHints(QPainter::Antialiasing);
 
