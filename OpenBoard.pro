OpenBoard-org/OpenBoard

commit 9c6074607989bfc3bae952c20951adc8ee9a16ab

Hack for building pre-release versions on Windows

OpenBoard.pro
@@ -91,8 +91,6 @@ UB_FONTS.files = resources/fonts
 UB_THIRDPARTY_INTERACTIVE.files = thirdparty/interactive
 
 DEFINES += NO_THIRD_PARTY_WARNINGS
-DEFINES += UBVERSION=\"\\\"$${LONG_VERSION}\"\\\" \
-   UBVERSION_RC=$$VERSION_RC
 ALPHA_BETA_STR = $$find(VERSION, "[ab]")
 count(ALPHA_BETA_STR, 1):DEFINES += PRE_RELEASE
 BUILD_DIR = build
@@ -140,6 +138,19 @@ win32 {
    DEFINES += NOMINMAX # avoids compilation error in qdatetime.h
 
 
+   # Windows doesn't support file versions with more than 4 fields, so
+   # we omit the build number (which is only used for pre-release versions
+   # anyway)
+
+   VERSION_RC = $$VERSION_MAJ,$$VERSION_MIN,$$VERSION_PATCH,$$VERSION_TYPE
+   VERSION_RC = $$replace(VERSION_RC, "a", "160") # 0xA0
+   VERSION_RC = $$replace(VERSION_RC, "b", "176") # 0xB0
+   VERSION_RC = $$replace(VERSION_RC, "rc", "192" ) # 0xC0
+   VERSION_RC = $$replace(VERSION_RC, "r", "240") # 0xF0
+
+
+   DEFINES += UBVERSION=\"\\\"$${VERSION}\"\\\" \
+        UBVERSION_RC=$$VERSION_RC
 }
 
 macx {

commit 13a471168a5b970beb28c6d6316eef1685671d99

Fixed build error on Linux caused by 9c607460

OpenBoard.pro
@@ -91,6 +91,8 @@ UB_FONTS.files = resources/fonts
 UB_THIRDPARTY_INTERACTIVE.files = thirdparty/interactive
 
 DEFINES += NO_THIRD_PARTY_WARNINGS
+DEFINES += UBVERSION=\"\\\"$${LONG_VERSION}\"\\\" \
+   UBVERSION_RC=$$VERSION_RC
 ALPHA_BETA_STR = $$find(VERSION, "[ab]")
 count(ALPHA_BETA_STR, 1):DEFINES += PRE_RELEASE
 BUILD_DIR = build