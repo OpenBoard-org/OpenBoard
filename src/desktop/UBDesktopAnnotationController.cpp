a8076a2be6927ad76e2bce19c97311b6e34c9d6a
Craig Watson <craig@watsons.ch>
2016-09-05 02:29:02 +0200

    Make sure that drop shadows are deactivated in desktop mode
    On OS X, making annotations in desktop mode then switching to board
    mode and back could cause shadows to be drawn around every stroke, and
    these persisted after erasing the strokes (though they did disappear
    upon switching to board mode and back again).

--- src/desktop/UBDesktopAnnotationController.cpp
+++ src/desktop/UBDesktopAnnotationController.cpp
@@ -85,7 +85,7 @@ UBDesktopAnnotationController::UBDesktopAnnotationController(QObject *parent, UB
 #ifdef Q_OS_OSX
     mTransparentDrawingView->setAttribute(Qt::WA_MacNoShadow, true);
 #endif
-    mTransparentDrawingView->setWindowFlags(Qt::FramelessWindowHint | Qt::WindowStaysOnTopHint | Qt::Window);
+    mTransparentDrawingView->setWindowFlags(Qt::FramelessWindowHint | Qt::WindowStaysOnTopHint | Qt::Window | Qt::NoDropShadowWindowHint);
     mTransparentDrawingView->setCacheMode(QGraphicsView::CacheNone);
     mTransparentDrawingView->resize(UBApplication::desktop()->width(), UBApplication::desktop()->height());
 
