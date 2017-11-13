d4a3a1984544be3a719c0ea0e06c93b3b641be5b
Clément Fauconnier <fc86@outlook.fr>
2017-11-13 14:35:42 +0100

    same behavior as Marker Circle for pen circle

--- src/board/UBBoardView.cpp
+++ src/board/UBBoardView.cpp
@@ -1719,12 +1719,7 @@ void UBBoardView::setToolCursor (int tool)
     switch (tool)
     {
     case UBStylusTool::Pen:
-        if (UBSettings::settings()->showPenPreviewCircle->get().toBool() &&
-            UBSettings::settings()->currentPenWidth() >= UBSettings::settings()->penPreviewFromSize->get().toInt()) {
-            controlViewport->setCursor (QCursor (Qt::BlankCursor));
-        } else {
-            controlViewport->setCursor (UBResources::resources ()->penCursor);
-        }
+        controlViewport->setCursor (UBResources::resources ()->penCursor);
         break;
     case UBStylusTool::Eraser:
         controlViewport->setCursor (UBResources::resources ()->eraserCursor);
