e228fb19e5dcff974883467f904580d0eafaab1c
Clément Fauconnier <fc86@outlook.fr>
2018-10-02 09:56:16 +0200

    updated Copyright to 2018

--- src/core/UBForeignObjectsHandler.h
+++ src/core/UBForeignObjectsHandler.h
@@ -1,23 +1,28 @@
 /*
-* Copyright (C) 2010-2013 Groupement d'Intérêt Public pour l'Education Numérique en Afrique (GIP ENA)
-*
-* This file is part of Open-Sankoré.
-*
-* Open-Sankoré is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, version 3 of the License,
-* with a specific linking exception for the OpenSSL project's
-* "OpenSSL" library (or with modified versions of it that use the
-* same license as the "OpenSSL" library).
-*
-* Open-Sankoré is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with Open-Sankoré.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ * Copyright (C) 2015-2016 Département de l'Instruction Publique (DIP-SEM)
+ *
+ * Copyright (C) 2013 Open Education Foundation
+ *
+ * Copyright (C) 2010-2013 Groupement d'Intérêt Public pour
+ * l'Education Numérique en Afrique (GIP ENA)
+ *
+ * This file is part of OpenBoard.
+ *
+ * OpenBoard is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License,
+ * with a specific linking exception for the OpenSSL project's
+ * "OpenSSL" library (or with modified versions of it that use the
+ * same license as the "OpenSSL" library).
+ *
+ * OpenBoard is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenBoard. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #ifndef UBFOREIGHNOBJECTSHANDLER_H
 #define UBFOREIGHNOBJECTSHANDLER_H
