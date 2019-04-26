d7b0e253e72e857efff74733594fc84d807d25fb
Jens Küspert <jens.kuespert@gmx.de>
2019-04-26 21:50:30 +0200

    initiale Darstellung der Farben wieder korrigiert

--- src/core/UBApplication.cpp
+++ src/core/UBApplication.cpp
@@ -354,8 +354,8 @@ int UBApplication::exec(const QString& pFileToImport)
     else
         applicationController->showBoard();
 
-    emit UBDrawingController::drawingController()->colorPaletteChanged();
     UBDrawingController::drawingController()->setStylusTool((int)UBStylusTool::Pen);
+    emit UBDrawingController::drawingController()->colorPaletteChanged();
 
     onScreenCountChanged(1);
     connect(desktop(), SIGNAL(screenCountChanged(int)), this, SLOT(onScreenCountChanged(int)));
