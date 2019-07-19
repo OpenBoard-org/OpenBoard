efbba6dce45665d3ce95246da25f7e7ae77442d9
Clément Fauconnier <fc86@outlook.fr>
2019-07-19 09:50:14 +0200

    remove obsolete connect with new qtsingleapplication

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -129,9 +129,6 @@ int main(int argc, char *argv[])
         }
     }
 
-
-    QObject::connect(&app, SIGNAL(messageReceived(const QString&, QObject*)), &app, SLOT(handleOpenMessage(const QString&)));
-
     qDebug() << "file name argument" << fileToOpen;
     int result = app.exec(fileToOpen);
 
