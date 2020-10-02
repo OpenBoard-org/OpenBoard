781066b373a24c8dae76e41f3a4cd2a03c645df2
Clément Fauconnier <fc86@outlook.fr>
2020-10-02 13:03:09 +0200

    osx play was started immediately on video drop

--- src/domain/UBGraphicsMediaItemDelegate.cpp
+++ src/domain/UBGraphicsMediaItemDelegate.cpp
@@ -243,12 +243,12 @@ void UBGraphicsMediaItemDelegate::mediaStatusChanged(QMediaPlayer::MediaStatus s
         mMediaControl->totalTimeChanged(delegated()->mediaDuration());
 
     // At the beginning of the video, play/pause to load and display the first frame
-    if ((status == QMediaPlayer::LoadedMedia || status == QMediaPlayer::BufferedMedia)
-            && delegated()->mediaPosition() == delegated()->initialPos()
-            && !delegated()->isStopped()) {
-        delegated()->play();
-        delegated()->pause();
-    }
+//    if ((status == QMediaPlayer::LoadedMedia || status == QMediaPlayer::BufferedMedia)
+//            && delegated()->mediaPosition() == delegated()->initialPos()
+//            && !delegated()->isStopped()) {
+//        delegated()->play();
+//        delegated()->pause();
+//    }
 
     // At the end of the video, make sure the progress bar doesn't autohide
     if (status == QMediaPlayer::EndOfMedia)
