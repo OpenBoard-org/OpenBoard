0cd02498b796082dedeb4725295951ea553d782f
letsfindaway <me@letsfindaway.de>
2023-05-06 18:37:01 +0200

    feat: add buttons to rotate camera image
    - add a left and right rotate button
    - useful when working with portrait media, e.g. book pages
    - fix color of play button
    - increase opacity of play/pause buttons

--- resources/library/applications/Camera.wgt/js/camera.js
+++ resources/library/applications/Camera.wgt/js/camera.js
@@ -1,4 +1,4 @@
-function init() {
+async function init() {
   if (!"mediaDevices" in navigator ||
       !"getUserMedia" in navigator.mediaDevices) {
     handleError("Camera API is not available in your browser");
@@ -8,8 +8,11 @@ function init() {
   // get page elements
   const video = document.querySelector("#video");
   const buttons = document.querySelector("#buttons");
+  const buttonsLeft = document.querySelector("#buttons-left");
   const size = document.querySelector("#size");
   const snapshot = document.querySelector("#snapshot");
+  const rotleft = document.querySelector("#rotleft");
+  const rotright = document.querySelector("#rotright");
   const canvas = document.querySelector("#canvas");
   const nocamera = document.querySelector("#nocamera");
   const videoSelect = document.querySelector('select#videoSource');
@@ -21,10 +24,14 @@ function init() {
 
   // current video stream
   let videoStream;
+  
+  // orientation
+  let orientation = await sankore.async.preference("orientation", "0");
 
   // connect hover slots
   window.widget.onenter.connect(() => { 
     buttons.style.display = "flex";
+    buttonsLeft.style.display = "flex";
     if (videoStream) {
       (playing ? pause : play).style.display = "block";
     }
@@ -32,6 +39,7 @@ function init() {
   
   window.widget.onleave.connect(() => { 
     buttons.style.display = "none"; 
+    buttonsLeft.style.display = "none";
     pause.style.display = "none";
     play.style.display = "none";
   });
@@ -42,9 +50,26 @@ function init() {
 
   // resize widget to video aspect ratio
   video.onresize = () => {
-      let width = window.innerWidth;
-      let height = width * video.videoHeight / video.videoWidth;
-      sankore.resize(width, height);
+      let size = layoutVideo();
+      sankore.resize(size.width, size.height);
+  };
+
+  // adjust video size when widget is resized
+  window.onresize = layoutVideo;
+
+  // rotate
+  rotleft.onclick = () => {
+    orientation = (orientation + 3) % 4;
+    sankore.setPreference("orientation", orientation);
+    let size = layoutVideo();
+    sankore.resize(size.width, size.height);
+  };
+
+  rotright.onclick = () => {
+    orientation = (orientation + 1) % 4;
+    sankore.setPreference("orientation", orientation);
+    let size = layoutVideo();
+    sankore.resize(size.width, size.height);
   };
 
   // play/pause
@@ -65,11 +90,70 @@ function init() {
   // take snapshot
   snapshot.onclick = (event) => {
     event.cancelBubble = true;
-    canvas.width = size.value;
-    canvas.height = canvas.width * video.videoHeight / video.videoWidth;
-    canvas.getContext("2d").drawImage(video, 0, 0, video.videoWidth, video.videoHeight, 0, 0, canvas.width, canvas.height);
-    var data = canvas.toDataURL("image/jpeg");
+
+    if (orientation % 2) {
+      canvas.height = size.value;
+      canvas.width = canvas.height * video.videoHeight / video.videoWidth;
+    } else {
+      canvas.width = size.value;
+      canvas.height = canvas.width * video.videoHeight / video.videoWidth;
+    }
+
+    let ctx = canvas.getContext("2d");
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.translate(canvas.width / 2, canvas.height / 2);
+    ctx.rotate(orientation * Math.PI / 2);
+
+    if (orientation % 2) {
+      ctx.translate(-canvas.height / 2, -canvas.width / 2);
+      ctx.drawImage(video, 0, 0, video.videoWidth, video.videoHeight, 0, 0, canvas.height, canvas.width);
+    } else {
+      ctx.translate(-canvas.width / 2, -canvas.height / 2);
+      ctx.drawImage(video, 0, 0, video.videoWidth, video.videoHeight, 0, 0, canvas.width, canvas.height);
+    }
+
+    let data = canvas.toDataURL("image/jpeg");
     sankore.addObject(data, 0, 0, 0, 0, false);
+  };
+
+  // layout video
+  function layoutVideo() {
+    let longWindow = Math.max(window.innerHeight, window.innerWidth);
+    let width;
+    let height
+
+    video.style.transform = "rotate(" + 90 * orientation + "deg)";
+
+    if (orientation % 2 == 0) {
+      // for landscape:
+      // use longest side for width
+      // adapt the height according to aspect ratio
+      width = longWindow;
+      height = width * video.videoHeight / video.videoWidth;
+
+      video.style.width = width + "px";
+      video.style.height = height + "px";
+      video.style.top = "0px";
+      video.style.left = "0px";
+    } else {
+      // for portrait:
+      // use longest side for height
+      // adapt width according to aspect ratio
+      height = longWindow;
+      width = height * video.videoHeight / video.videoWidth;
+
+      video.style.width = height + "px";
+      video.style.height = width + "px";
+      let offset = (height - width) / 2;
+      video.style.top = offset + "px";
+      video.style.left = -offset + "px";
+    }
+
+    return {
+      'width': width,
+      'height': height
+    };
   };
 
   // stop video stream
