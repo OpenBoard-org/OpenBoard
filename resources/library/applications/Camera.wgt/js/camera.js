f142a263759ccaeec89acfedaa5529cb19126071
letsfindaway <me@letsfindaway.de>
2023-05-10 10:37:51 +0200

    feat: usability improvements
    - icons only in the UI
    - hoverable dropdown menus for camera and snapshot size selection
    - update list of cameras when hovering the camera select icon
    - show snapshot image sizes in size selection menu
    - build list of available snapshot sizes in JS code
    - use CSS tooltips for icons to control positioning
    - improve icons

--- resources/library/applications/Camera.wgt/js/camera.js
+++ resources/library/applications/Camera.wgt/js/camera.js
@@ -1,3 +1,12 @@
+// size options
+const sizeOptions = [
+  { 'label': "S", 'size': 320 },
+  { 'label': "M", 'size': 640 },
+  { 'label': "L", 'size': 960 },
+  { 'label': "XL", 'size': 1280 }
+];
+
+
 async function init() {
   if (!"mediaDevices" in navigator ||
       !"getUserMedia" in navigator.mediaDevices) {
@@ -6,52 +15,90 @@ async function init() {
   }
 
   // get page elements
-  const video = document.querySelector("#video");
-  const buttons = document.querySelector("#buttons");
-  const buttonsLeft = document.querySelector("#buttons-left");
-  const size = document.querySelector("#size");
-  const snapshot = document.querySelector("#snapshot");
-  const rotleft = document.querySelector("#rotleft");
-  const rotright = document.querySelector("#rotright");
-  const canvas = document.querySelector("#canvas");
-  const nocamera = document.querySelector("#nocamera");
-  const videoSelect = document.querySelector('select#videoSource');
-  const play = document.querySelector("#play");
-  const pause = document.querySelector("#pause");
+  const video         = document.getElementById("video");
+  const buttons       = document.getElementById("buttons");
+  const buttonsLeft   = document.getElementById("buttons-left");
+  const snapshot      = document.getElementById("snapshot");
+  const rotleft       = document.getElementById("rotleft");
+  const rotright      = document.getElementById("rotright");
+  const canvas        = document.getElementById("canvas");
+  const nocamera      = document.getElementById("nocamera");
+  const selectCamera  = document.getElementById("selectCamera");
+  const videoSelect   = document.getElementById("videoSelect");
+  const sizeSelect    = document.getElementById("sizeSelect");
+  const sizeIcon      = document.getElementById("sizeIcon");
+  const play          = document.getElementById("play");
+  const pause         = document.getElementById("pause");
+
+  // current camera;
+  let camera;
+
+  // current camera label
+  let cameraLabel;
 
   // camera status
   let playing = true;
 
   // current video stream
   let videoStream;
-  
+
   // orientation
   let orientation = await sankore.async.preference("orientation", "0");
 
+  // snapshot size
+  let size;
+
+  // setup size options
+  for (const sizeOption of sizeOptions) {
+    const input = document.createElement('input');
+    input.setAttribute("type", "radio");
+    input.setAttribute("name", "size");
+    input.setAttribute("id", sizeOption.label);
+    input.value = sizeOption.size;
+
+    const label = document.createElement('label');
+    label.appendChild(input);
+    const name = document.createTextNode(sizeOption.label);
+    label.appendChild(name);
+
+    const div = document.createElement('div');
+    div.onclick = () => {
+      setSize(sizeOption.label);
+    };
+    div.appendChild(label);
+
+    sizeSelect.appendChild(div);
+  }
+
+  size = document.getElementById("M");
+  size.checked = true;
+
   // connect hover slots
-  window.widget.onenter.connect(() => { 
+  window.widget.onenter.connect(() => {
     buttons.style.display = "flex";
     buttonsLeft.style.display = "flex";
     if (videoStream) {
       (playing ? pause : play).style.display = "block";
     }
   });
-  
-  window.widget.onleave.connect(() => { 
-    buttons.style.display = "none"; 
+
+  window.widget.onleave.connect(() => {
+    buttons.style.display = "none";
     buttonsLeft.style.display = "none";
     pause.style.display = "none";
     play.style.display = "none";
   });
-  
+
   // connect event handler
-  // reassign selected stream
-  videoSelect.onchange = getStream;
+  // rescan camera list
+  selectCamera.onmouseenter = () => {
+    getDevices().then(gotDevices);
+  }
 
   // resize widget to video aspect ratio
   video.onresize = () => {
-      let size = layoutVideo();
-      sankore.resize(size.width, size.height);
+    let videoSize = layoutVideo();
+    sankore.resize(videoSize.width, videoSize.height);
   };
 
   // adjust video size when widget is resized
@@ -61,15 +108,15 @@ async function init() {
   rotleft.onclick = () => {
     orientation = (orientation + 3) % 4;
     sankore.setPreference("orientation", orientation);
-    let size = layoutVideo();
-    sankore.resize(size.width, size.height);
+    let videoSize = layoutVideo();
+    sankore.resize(videoSize.width, videoSize.height);
   };
 
   rotright.onclick = () => {
     orientation = (orientation + 1) % 4;
     sankore.setPreference("orientation", orientation);
-    let size = layoutVideo();
-    sankore.resize(size.width, size.height);
+    let videoSize = layoutVideo();
+    sankore.resize(videoSize.width, videoSize.height);
   };
 
   // play/pause
@@ -79,14 +126,14 @@ async function init() {
     play.style.display = "none";
     pause.style.display = "block";
   };
-  
+
   pause.onclick = () => {
     video.pause();
     playing = false;
     pause.style.display = "none";
     play.style.display = "block";
   };
-  
+
   // take snapshot
   snapshot.onclick = (event) => {
     event.cancelBubble = true;
@@ -119,13 +166,17 @@ async function init() {
 
   // layout video
   function layoutVideo() {
+    if (video.videoHeight === 0 || video.videoWidth === 0) {
+      return;
+    }
+
     let longWindow = Math.max(window.innerHeight, window.innerWidth);
     let width;
-    let height
+    let height;
 
     video.style.transform = "rotate(" + 90 * orientation + "deg)";
 
-    if (orientation % 2 == 0) {
+    if (orientation % 2 === 0) {
       // for landscape:
       // use longest side for width
       // adapt the height according to aspect ratio
@@ -150,6 +201,16 @@ async function init() {
       video.style.left = -offset + "px";
     }
 
+    // update snapshot size entries
+    for (entry of sizeSelect.children) {
+      const pixels = entry.firstChild.firstChild.value;
+      const factor = pixels / longWindow;
+      const x = Math.floor(width * factor);
+      const y = Math.floor(height * factor);
+      const label = entry.firstChild.firstChild.id + " (" + x + "x" + y + ")";
+      entry.firstChild.childNodes[1].textContent = label;
+    }
+
     return {
       'width': width,
       'height': height
@@ -164,7 +225,7 @@ async function init() {
       });
     }
   }
-  
+
   // request list of media devices
   function getDevices() {
     return navigator.mediaDevices.enumerateDevices();
@@ -172,31 +233,63 @@ async function init() {
 
   // extract video devices and build selection options
   function gotDevices(deviceInfos) {
+    // delete list
+    while (videoSelect.hasChildNodes()) {
+      videoSelect.removeChild(videoSelect.firstChild);
+    }
+
+    // populate list
     for (const deviceInfo of deviceInfos) {
-      const option = document.createElement('option');
-      option.value = deviceInfo.deviceId;
       if (deviceInfo.kind === 'videoinput') {
-        option.text = deviceInfo.label || `Camera ${videoSelect.length + 1}`;
-        videoSelect.appendChild(option);
+        const input = document.createElement('input');
+        input.setAttribute("type", "radio");
+        input.setAttribute("name", "source");
+        input.setAttribute("id", deviceInfo.deviceId);
+        input.value = deviceInfo.deviceId;
+
+        const label = document.createElement('label');
+        label.appendChild(input);
+        const text = deviceInfo.label || `Camera ${videoSelect.length + 1}`;
+        const name = document.createTextNode(text);
+        label.appendChild(name);
+
+        const div = document.createElement('div');
+        div.onclick = () => {
+          setCamera(deviceInfo.deviceId);
+        };
+        div.appendChild(label);
+
+        videoSelect.appendChild(div);
       }
     }
-    // show selection if more than one video device
-    if (videoSelect.options.length <= 1) {
-      videoSelect.style.display = "none";
+
+    // select previously selected video source
+    let selected = undefined;
+
+    if (cameraLabel) {
+      selected = [...videoSelect.children].find(entry => entry.firstChild.textContent === cameraLabel);
+    }
+
+    if (selected) {
+      selected.firstChild.firstChild.checked = true;
+    } else {
+      // camera no longer available
+      handleError();
     }
   }
 
   // request stream for selected device
   function getStream() {
     stopVideoStream();
-    const videoSource = videoSelect.value;
+    const videoSource = camera;
     const constraints = {
       video: {
         deviceId: videoSource ? {exact: videoSource} : undefined,
-        width: { ideal: 1920 }, 
-        height: { ideal: 1080 }  
+        width: { ideal: 1920 },
+        height: { ideal: 1080 }
       }
     };
+
     return navigator.mediaDevices.getUserMedia(constraints).
       then(gotStream).catch(handleError);
   }
@@ -204,18 +297,46 @@ async function init() {
   // assign stream to video element
   function gotStream(stream) {
     videoStream = stream;
-    videoSelect.selectedIndex = [...videoSelect.options].
-      findIndex(option => option.text === stream.getVideoTracks()[0].label);
+    cameraLabel = stream.getVideoTracks()[0].label;
+    let selected = [...videoSelect.children].find(entry => entry.firstChild.textContent === cameraLabel);
+    if (selected) {
+      selected.firstChild.firstChild.checked = true;
+    }
+
     video.srcObject = stream;
+    video.style.display = "unset";
+    nocamera.style.display = "none";
   }
 
   // display "no camera" symbol
   function handleError(error) {
     videoStream = undefined;
+    camera = undefined;
+    cameraLabel = undefined;
     video.style.display = "none";
+    pause.style.display = "none";
+    play.style.display = "none";
     nocamera.style.display = "flex";
   }
 
+  // change size
+  function setSize(id) {
+    size = document.getElementById(id);
+    size.checked = true;
+    sizeIcon.textContent = id;
+  }
+
+  // select camera
+  function setCamera(id) {
+    const input = document.getElementById(id);
+
+    if (input) {
+      input.checked = true;
+      camera = input.id;
+      getStream();
+    }
+  }
+
   // initialize
-  getStream().then(getDevices).then(gotDevices);
+  getDevices().then(gotDevices).then(getStream);
 }
