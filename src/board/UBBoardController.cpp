OpenBoard-org/OpenBoard

commit 3e76f160bf4f4a4579d96363739f81f6d36d4101

let the user create blank documents if he/she wants to + take rename into account + improved scene modifications handling

src/board/UBBoardController.cpp
@@ -151,7 +151,10 @@ void UBBoardController::init()
 
     std::shared_ptr<UBDocumentProxy> doc = UBPersistenceManager::persistenceManager()->createNewDocument();
 
-    setActiveDocumentScene(doc);
+    if (doc)
+    {
+        mInitialDocumentScene = setActiveDocumentScene(doc);
+    }
 
     connect(UBApplication::displayManager, &UBDisplayManager::screenRolesAssigned, this, [this](){
         initBackgroundGridSize();
@@ -1543,12 +1546,12 @@ UBItem *UBBoardController::downloadFinished(bool pSuccess, QUrl sourceUrl, QUrl 
     return NULL;
 }
 
-void UBBoardController::setActiveDocumentScene(int pSceneIndex)
+std::shared_ptr<UBGraphicsScene> UBBoardController::setActiveDocumentScene(int pSceneIndex)
 {
-    setActiveDocumentScene(selectedDocument(), pSceneIndex);
+    return setActiveDocumentScene(selectedDocument(), pSceneIndex);
 }
 
-void UBBoardController::setActiveDocumentScene(std::shared_ptr<UBDocumentProxy> pDocumentProxy, const int pSceneIndex, bool forceReload, bool onImport)
+std::shared_ptr<UBGraphicsScene> UBBoardController::setActiveDocumentScene(std::shared_ptr<UBDocumentProxy> pDocumentProxy, const int pSceneIndex, bool forceReload, bool onImport)
 {
     UBApplication::setOverrideCursor(QCursor(Qt::WaitCursor));
     saveViewState();
@@ -1583,9 +1586,9 @@ void UBBoardController::setActiveDocumentScene(std::shared_ptr<UBDocumentProxy> 
 
         updateSystemScaleFactor();
 
+        disconnect(UBApplication::undoStack.data(), SIGNAL(indexChanged(int)), mControlView->scene().get(), SLOT(updateSelectionFrameWrapper(int)));
         mControlView->setScene(mActiveScene.get());
-        disconnect(mControlView, SIGNAL(mouseReleased()), mActiveScene.get(), SLOT(updateSelectionFrame()));
-        connect(mControlView, SIGNAL(mouseReleased()), mActiveScene.get(), SLOT(updateSelectionFrame()));
+        connect(UBApplication::undoStack.data(), SIGNAL(indexChanged(int)), mControlView->scene().get(), SLOT(updateSelectionFrameWrapper(int)));
 
         mDisplayView->setScene(mActiveScene.get());
         mActiveScene->setBackgroundZoomFactor(mControlView->transform().m11());
@@ -1630,6 +1633,8 @@ void UBBoardController::setActiveDocumentScene(std::shared_ptr<UBDocumentProxy> 
         qWarning() << "could not load document scene : '" << pDocumentProxy->persistencePath() << "', page index : " << pSceneIndex;
     }
     UBApplication::restoreOverrideCursor();
+
+    return targetScene;
 }
 
 
@@ -1945,16 +1950,31 @@ void UBBoardController::lastWindowClosed()
 {
     if (!mCleanupDone)
     {
-        if (selectedDocument()->pageCount() == 1 && (!mActiveScene || mActiveScene->isEmpty()))
+        if (initialDocumentScene() && initialDocumentScene()->document())
         {
-            UBPersistenceManager::persistenceManager()->deleteDocument(selectedDocument());
+            if (initialDocumentScene()->isEmpty() && (initialDocumentScene()->document()->documentDate() == initialDocumentScene()->document()->lastUpdate()))
+            {
+                // intial scene or document have not been modified at all, so we can delete the document.
+                UBPersistenceManager::persistenceManager()->deleteDocument(initialDocumentScene()->document());
+
+                //if current scene is not the initial document scene, we still need to persist it to ensure no data can be lost this way
+                if (activeScene() != initialDocumentScene())
+                {
+                    persistCurrentScene();
+                }
+            }
+            else
+            {
+                // if intial scene or document changed, then rather the initial document scene is the current scene,
+                // or current scene changed and initial document scene has already been persisted.
+                // Now, we just persist the current scene before closing the app, to ensure no data can be lost this way.
+                persistCurrentScene();
+            }
         }
         else
-        {
+        { //should not happen ?
             persistCurrentScene();
         }
-
-        UBPersistenceManager::persistenceManager()->purgeEmptyDocuments();
 
         mCleanupDone = true;
     }

src/adaptors/UBSvgSubsetAdaptor.cpp
@@ -246,7 +246,7 @@ std::shared_ptr<UBGraphicsScene> UBSvgSubsetAdaptor::loadScene(std::shared_ptr<U
 {
     UBApplication::showMessage(QObject::tr("Loading scene (%1/%2)").arg(pageIndex+1).arg(proxy->pageCount()));
     QString fileName = proxy->persistencePath() + UBFileSystemUtils::digitFileFormat("/page%1.svg", pageIndex);
-    qDebug() << fileName;
+    qInfo() << "loading scene. Filename is : " << fileName;
     QFile file(fileName);
 
     if (file.exists())
@@ -1514,7 +1514,7 @@ bool UBSvgSubsetAdaptor::UBSvgSubsetWriter::persistScene(std::shared_ptr<UBDocum
 
     if (!file.open(QIODevice::WriteOnly | QIODevice::Truncate))
     {
-        qCritical() << "cannot open " << fileName << " for writing ...";
+        qCritical() << "cannot open " << fileName << " for writing. Error : " << file.errorString();
         return false;
     }
     file.write(buffer.data());

src/board/UBBoardController.h
@@ -182,8 +182,8 @@ class UBBoardController : public UBDocumentContainer
         void findUniquesItems(const QUndoCommand *parent, QSet<QGraphicsItem *> &items);
         void ClearUndoStack();
 
-        void setActiveDocumentScene(std::shared_ptr<UBDocumentProxy> pDocumentProxy, int pSceneIndex = 0, bool forceReload = false, bool onImport = false);
-        void setActiveDocumentScene(int pSceneIndex);
+        std::shared_ptr<UBGraphicsScene> setActiveDocumentScene(std::shared_ptr<UBDocumentProxy> pDocumentProxy, int pSceneIndex = 0, bool forceReload = false, bool onImport = false);
+        std::shared_ptr<UBGraphicsScene> setActiveDocumentScene(int pSceneIndex);
 
         void moveSceneToIndex(int source, int target);
         void duplicateScene(int index);
@@ -194,6 +194,11 @@ class UBBoardController : public UBDocumentContainer
 
         QString actionGroupText(){ return mActionGroupText;}
         QString actionUngroupText(){ return mActionUngroupText;}
+
+        std::shared_ptr<UBGraphicsScene> initialDocumentScene()
+        {
+            return mInitialDocumentScene;
+        }
 
     public slots:
         void showDocumentsDialog();
@@ -322,6 +327,7 @@ class UBBoardController : public UBDocumentContainer
         int mMovingSceneIndex;
         QString mActionGroupText;
         QString mActionUngroupText;
+        std::shared_ptr<UBGraphicsScene> mInitialDocumentScene;
 
         QTimer *mAutosaveTimer;
 

src/board/UBBoardView.cpp
@@ -1585,7 +1585,7 @@ void UBBoardView::mouseReleaseEvent (QMouseEvent *event)
     setMovingItem(NULL);
 
     mLongPressTimer.stop();
-    emit mouseReleased();
+    scene()->updateSelectionFrame();
 }
 
 void UBBoardView::forcedTabletRelease ()

src/core/UBApplicationController.cpp
@@ -450,8 +450,7 @@ void UBApplicationController::showDocument()
 
     if (UBApplication::boardController)
     {
-        if (UBApplication::boardController->activeScene()->isModified())
-            UBApplication::boardController->persistCurrentScene();
+        UBApplication::boardController->persistCurrentScene();
 
         UBApplication::boardController->hide();
     }

src/core/UBPersistenceManager.cpp
@@ -650,9 +650,6 @@ std::shared_ptr<UBDocumentProxy> UBPersistenceManager::createNewDocument(const Q
                                                       , bool promptDialogIfExists)
 {
     std::shared_ptr<UBDocumentProxy> resultDoc = createDocument(pGroupName, pName, withEmptyPage, directory, pageCount, promptDialogIfExists);
-    if (resultDoc) {
-        mDocumentTreeStructureModel->markDocumentAsNew(resultDoc);
-    }
 
     return resultDoc;
 }
@@ -726,6 +723,7 @@ std::shared_ptr<UBDocumentProxy> UBPersistenceManager::createDocumentFromDir(con
 
 void UBPersistenceManager::deleteDocument(std::shared_ptr<UBDocumentProxy> pDocumentProxy)
 {
+    qWarning() << "deleting dir with path: " << pDocumentProxy->persistencePath();
     checkIfDocumentRepositoryExists();
 
     if (QFileInfo(pDocumentProxy->persistencePath()).exists())
@@ -1143,29 +1141,23 @@ void UBPersistenceManager::persistDocumentScene(std::shared_ptr<UBDocumentProxy>
     QDir dir(pDocumentProxy->persistencePath());
     dir.mkpath(pDocumentProxy->persistencePath());
 
-    if (pDocumentProxy->isModified())
+    persistDocumentMetadata(pDocumentProxy, forceImmediateSaving);
+
+    if(forceImmediateSaving)
     {
-        persistDocumentMetadata(pDocumentProxy, forceImmediateSaving);
+        UBSvgSubsetAdaptor::persistScene(pDocumentProxy, pScene, pSceneIndex);
+    }
+    else
+    {
+       std::shared_ptr<UBGraphicsScene> copiedScene = pScene->sceneDeepCopy();
+       mWorker->saveScene(pDocumentProxy, copiedScene.get(), pSceneIndex);
+
+       // keep copiedScene alive until saving is finished
+       mScenesToSave.append(copiedScene);
     }
 
-    if (pScene->isModified())
-    {
-        if(forceImmediateSaving)
-        {
-            UBSvgSubsetAdaptor::persistScene(pDocumentProxy, pScene, pSceneIndex);
-        }
-        else
-        {
-           std::shared_ptr<UBGraphicsScene> copiedScene = pScene->sceneDeepCopy();
-           mWorker->saveScene(pDocumentProxy, copiedScene.get(), pSceneIndex);
-
-           // keep copiedScene alive until saving is finished
-           mScenesToSave.append(copiedScene);
-        }
-
-        UBThumbnailAdaptor::persistScene(pDocumentProxy, pScene, pSceneIndex);
-        pScene->setModified(false);
-    }
+    UBThumbnailAdaptor::persistScene(pDocumentProxy, pScene, pSceneIndex);
+    pScene->setModified(false);
 
     mSceneCache.insert(pDocumentProxy, pSceneIndex, pScene);
 }
@@ -1337,25 +1329,6 @@ bool UBPersistenceManager::isEmpty(std::shared_ptr<UBDocumentProxy> pDocumentPro
     }
 
     return empty;
-}
-
-
-void UBPersistenceManager::purgeEmptyDocuments()
-{
-    QList<std::shared_ptr<UBDocumentProxy>> toBeDeleted;
-
-    foreach(std::shared_ptr<UBDocumentProxy> docProxy, mDocumentTreeStructureModel->newDocuments())
-    {
-        if (isEmpty(docProxy))
-        {
-            toBeDeleted << docProxy;
-        }
-    }
-
-    foreach(std::shared_ptr<UBDocumentProxy> docProxy, toBeDeleted)
-    {
-        deleteDocument(docProxy);
-    }
 }
 
 bool UBPersistenceManager::addFileToDocument(std::shared_ptr<UBDocumentProxy> pDocumentProxy,

src/core/UBPersistenceManager.h
@@ -142,7 +142,6 @@ class UBPersistenceManager : public QObject
         }
 
         virtual bool isEmpty(std::shared_ptr<UBDocumentProxy> pDocumentProxy);
-        virtual void purgeEmptyDocuments();
 
         bool addGraphicsWidgetToDocument(std::shared_ptr<UBDocumentProxy> mDocumentProxy, QString path, QUuid objectUuid, QString& destinationPath);
         bool addFileToDocument(std::shared_ptr<UBDocumentProxy> pDocumentProxy, QString path, const QString& subdir,  QUuid objectUuid, QString& destinationPath, QByteArray* data = NULL);

src/core/UBPersistenceWorker.cpp
@@ -81,10 +81,8 @@ void UBPersistenceWorker::process()
             emit sceneLoaded(UBSvgSubsetAdaptor::loadSceneAsText(info.proxy,info.sceneIndex), info.proxy, info.sceneIndex);
         }
         else if (info.action == WriteMetadata) {
-            if (info.proxy->isModified()) {
-                UBMetadataDcSubsetAdaptor::persist(info.proxy);
-                emit metadataPersisted(info.proxy);
-            }
+            UBMetadataDcSubsetAdaptor::persist(info.proxy);
+            emit metadataPersisted(info.proxy);
         }
         mSemaphore.acquire();
     }while(!mReceivedApplicationClosing);

commit 1a64b535895bf6fbdb06badd58e9d1e5caedd5f8

fix: disconnect with nullptr receiver
- fix regression introduced with 3e76f16
- check receiver before disconnecting signal
- only disconnect when receiver is not-null

src/board/UBBoardController.cpp
@@ -1586,7 +1586,11 @@ std::shared_ptr<UBGraphicsScene> UBBoardController::setActiveDocumentScene(std::
 
         updateSystemScaleFactor();
 
-        disconnect(UBApplication::undoStack.data(), SIGNAL(indexChanged(int)), mControlView->scene().get(), SLOT(updateSelectionFrameWrapper(int)));
+        if (mControlView->scene())
+        {
+            disconnect(UBApplication::undoStack.data(), SIGNAL(indexChanged(int)), mControlView->scene().get(), SLOT(updateSelectionFrameWrapper(int)));
+        }
+
         mControlView->setScene(mActiveScene.get());
         connect(UBApplication::undoStack.data(), SIGNAL(indexChanged(int)), mControlView->scene().get(), SLOT(updateSelectionFrameWrapper(int)));
 