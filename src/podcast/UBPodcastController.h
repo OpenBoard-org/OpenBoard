2d9e0de05723c18cc372f9ab7f0590e79d955efd
letsfindaway <me@letsfindaway.de>
2025-07-21 17:17:14 +0200

    feat: podcast in desktop mode
    - use ScreenCast to record desktop
    - send images to encoder
    - create and terminate a transient stream when switching to
      desktop mode for the first time with recording disabled
    
    Signed-off-by: letsfindaway <me@letsfindaway.de>

--- src/podcast/UBPodcastController.h
+++ src/podcast/UBPodcastController.h
@@ -40,6 +40,9 @@ class UBGraphicsScene;
 class WebView;
 class UBPodcastRecordingPalette;
 
+class UBDesktopPortal;
+class UBPipewireSink;
+
 
 class UBPodcastController : public QObject
 {
@@ -188,6 +191,10 @@ class UBPodcastController : public QObject
 
         QString mPodcastRecordingPath;
 
+#ifdef Q_OS_LINUX
+        UBDesktopPortal* mPortal{nullptr};
+        UBPipewireSink* mPipewireSink{nullptr};
+#endif
 };
 
 #endif /* UBPODCASTCONTROLLER_H_ */
